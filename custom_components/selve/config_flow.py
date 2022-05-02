0d12353f21e42fa21025ddcf72c5307ce7a1a645
Kannnix2005 <stefan.altheimer@gmail.com>
2022-05-02 18:07:01 +0200

    fixed config flow

--- custom_components/selve/config_flow.py
+++ custom_components/selve/config_flow.py
@@ -60,7 +60,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
         if user_input is None:
             return self.async_show_form(
-                step_id="port", data_schema=STEP_USER_DATA_SCHEMA
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
             )
 
         errors = {}
@@ -78,7 +78,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             return self.async_create_entry(title=info["Gateway"], data=user_input)
 
         return self.async_show_form(
-            step_id="port", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
 
 
